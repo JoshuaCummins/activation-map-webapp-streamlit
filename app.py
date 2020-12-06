cd2790524e7a8bf5bf9c5c375984529433e95a37
JoshuaCummins <65562250+JoshuaCummins@users.noreply.github.com>
2020-12-05 18:32:33 -0800

    fixed image variable names

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if uploaded_file is not None:
 
 	imgs = Image.open(io.BytesIO(uploaded_file.read()))
 
-	img = np.array(image.resize((224, 224)))
+	img = np.array(imgs.resize((224, 224)))
 	x = preprocess_input(np.expand_dims(img, 0))
 	fmaps = model.predict(x)[0] # 7 x 7 x 2048
 
